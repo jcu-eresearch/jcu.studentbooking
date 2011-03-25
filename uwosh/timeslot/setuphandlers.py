4c9cac580bd6ea10b83716bfed67d897a2d9abbf
David Breitkreutz <david.breitkreutz@jcu.edu.au>
2011-03-25 14:19:50 +1000

    Adding upgrade step for migration to auto-closure functionality.

--- uwosh/timeslot/setuphandlers.py
+++ uwosh/timeslot/setuphandlers.py
@@ -1,5 +1,11 @@
 import logging
-from uwosh.timeslot.config import PROJECTNAME 
+
+from Products.CMFCore.utils import getToolByName
+from plone.app.workflow.remap import remap_workflow
+
+from uwosh.timeslot.config import PROJECTNAME
+
+PROFILE_ID = 'profile-uwosh.timeslot:default'
 
 def setupVarious(context):
     """Import step for configuration that is not handled in xml files.
@@ -19,3 +25,27 @@ def setupVarious(context):
     site.portal_catalog.manage_reindexIndex(ids=fields)
     logger.info("Reindexing EHS booking session content.")
 
+def upgrade_timeslot_workflow(context, logger=None):
+
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger('uwosh.timeslot')
+
+    #Run the workflow setup
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'workflow')
+
+    remap_workflow(context,
+                   type_ids=('Day', 'Time Slot'),
+                   chain='(Default)',
+                   state_map={'hidden': 'private'}
+                  )
+
+    remap_workflow(context,
+                   type_ids=('Day', 'Time Slot'),
+                   chain=('uwosh_timeslot_hidden_workflow',),
+                   state_map={'private': 'open',
+                              'pending': 'open',
+                              'published': 'open'}
+                  )
+    return
